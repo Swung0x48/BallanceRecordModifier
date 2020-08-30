77cb59dae6aeda0df7b3bdf52c9c45d4e133f9d2
Swung0x48 <swung0x48@outlook.com>
2020-08-30 23:47:05 +0800

    🎉 Now unit test has covered all code from ByteManipulator! 🎉

--- BallanceRecordModifier.UnitTest/ByteManipulatorUnitTest.cs
+++ BallanceRecordModifier.UnitTest/ByteManipulatorUnitTest.cs
@@ -8,20 +8,26 @@ namespace BallanceRecordModifier.UnitTest
 {
     public class ByteManipulatorUnitTest
     {
+        [Fact]
+        public void TestCreateFromNullArray()
+        {
+            Assert.Throws<NullReferenceException>(() => ByteManipulator.Create(null));
+        }
+
         [Theory]
         [InlineData("YiLB4gfG5kRGxySGwWOk7ww=", "DB_Highscore_Lv01")]
-        [InlineData("", "")]
+        [InlineData("gySv6WKGpgaEZ2Q=", "Mr. Default")]
         public void TestDecode(string sample, string expected)
         {
-            Assert.Equal(ByteManipulator.Decode(
-                    Convert.FromBase64String(sample)),
-                Encoding.ASCII.GetBytes(expected)
+            Assert.Equal(
+                Encoding.ASCII.GetBytes(expected),
+                ByteManipulator.Decode(
+                    Convert.FromBase64String(sample))
             );
         }
 
         [Theory]
         [InlineData("DB_Highscore_Lv01", "YiLB4gfG5kRGxySGwWOk7ww=")]
-        [InlineData("", "")]
         public void TestEncode(string sample, string expected)
         {
             Assert.Equal(ByteManipulator.Encode(
@@ -30,43 +36,21 @@ namespace BallanceRecordModifier.UnitTest
             );
         }
 
-        [Theory]
-        [InlineData(null, null)]
-        public void TestEncodeWithNullArray(byte[]? sample, byte[]? expected)
+        [Fact]
+        public void TestEncodeWithNullOrEmptyArray()
         {
-            try
-            {
-                ByteManipulator.Decode(sample);
-            }
-            catch (NullReferenceException)
-            {
-                Assert.Null(sample);
-            }
+            Assert.Throws<NullReferenceException>(() => ByteManipulator.Encode(null));
+            Assert.Throws<ArgumentNullException>(() => ByteManipulator.Encode(new byte[]{}));
+        }
+
+        [Fact]
+        public void TestDecodeWithNullOrEmptyArray()
+        {
+            Assert.Throws<NullReferenceException>(() => ByteManipulator.Decode(null));
+            Assert.Throws<ArgumentNullException>(() => ByteManipulator.Decode(new byte[]{}));
         }
 
         [Theory]
-        [InlineData(null)]
-        [InlineData(new byte[] { })]
-        [InlineData(new byte[] { 11, 22, 33 })]
-        public void TestDecodeWithNullArray(byte[]? sample)
-        {
-            try
-            {
-                ByteManipulator.Decode(sample);
-            }
-            catch (ArgumentNullException)
-            {
-                Assert.Equal(sample, new byte[] { });
-            }
-            catch (NullReferenceException)
-            {
-                Assert.Null(sample);
-            }
-        }
-
-        [Theory]
-        [InlineData(null, null)]
-        [InlineData(new byte[]{}, "")]
         [InlineData(new byte[] {0, 11, 22, 33}, "")]
         [InlineData(
             new byte[] {(byte)'a', (byte)'b', (byte)'c', 0, (byte)'d', (byte)'e'}, 
@@ -82,42 +66,129 @@ namespace BallanceRecordModifier.UnitTest
         )]
         public void TestReadString(byte[]? sample, string? expected)
         {
-            try
-            {
-                var bm = ByteManipulator.Create(ByteManipulator.Encode(sample));
-                Assert.Equal(bm.ReadString(), expected);
-            }
-            catch (NullReferenceException)
-            {
-                Assert.Null(sample);
-            }
+            var bm = ByteManipulator.Create(ByteManipulator.Encode(sample));
+            Assert.Equal(expected, bm.ReadString());
         }
 
         [Theory]
-        [InlineData("", new string[] {})]
         [InlineData("Hello\0World\0", new string[] {"Hello", "World"})]
+        [InlineData("Hello\0\0\0\0World\0\0", new string[] {"Hello", "", "", "", "World", ""})]
         [InlineData("Hello World\0", new string[] {"Hello World"})]
         [InlineData(
             "Hello\0from\0the\0other\0side", 
             new string[] {"Hello", "from", "the", "other", "side"})]
         public void TestMultipleReadString(string? sample, string[]? expected)
         {
-            try
+            string[] read = {};
+            ByteManipulator bm = ByteManipulator.Create(
+                ByteManipulator.Encode(
+                    Encoding.ASCII.GetBytes(sample)
+                ));
+            
+            for (var i = 0; i < expected.Length; i++) 
+            { 
+                read.Append(bm.ReadString());
+            }
+        }
+
+        [Fact]
+        public void TestReadStringOnExhaustedBytes()
+        {
+            var bm = ByteManipulator.Create(new byte[] {0, 1, 2, 3});
+            bm.ReadInt();
+            Assert.Throws<InvalidOperationException>(() => bm.ReadString());
+        }
+
+        [Theory]
+        [InlineData(new int[]{0, 1, 2, 3}, new int[]{0, 1, 2, 3})]
+        [InlineData(new int[]{4000, 3600, 3200}, new int[]{4000, 3600, 3200})]
+        public void TestReadInt(int[] samples, int[] expected)
+        {
+            List<byte> sampleBytes = new List<byte>();
+            foreach (var t in samples)
             {
-                string[] read = {};
-                ByteManipulator bm = ByteManipulator.Create(
-                    ByteManipulator.Encode(
-                        Encoding.ASCII.GetBytes(sample)
-                    ));
-                for (var i = 0; i < expected.Length; i++)
+                var bytes = BitConverter.GetBytes(t);
+                for (var j = 0; j < 4; j++)
                 {
-                    read.Append(bm.ReadString());
+                    sampleBytes.Add(bytes[j]);
                 }
             }
-            catch (NullReferenceException)
-            {
-                Assert.Null(sample);
+            Assert.Equal(samples.Length * 4, sampleBytes.Count);
+            
+            ByteManipulator bm = ByteManipulator.Create(ByteManipulator.Encode(sampleBytes.ToArray()));
+
+            foreach (var i in expected) 
+            { 
+                Assert.Equal(i, bm.ReadInt());
             }
+            
+        }
+
+        [Theory]
+        [InlineData(
+            "oA8AABAOAACADAAA8AoAAGAJAADQBwAAQAYAALAEAAAgAwAAkAEAAA==",
+            new int[] {4000, 3600, 3200, 2800, 2400, 2000, 1600, 1200, 800, 400}
+        )]
+        public void TestReadIntFromRaw(string? sample, int[]? expected)
+        {
+            var sampleBytes =  Convert.FromBase64String(sample);
+            var bm = ByteManipulator.Create(ByteManipulator.Encode(sampleBytes));
+
+            foreach (var i in expected)
+            {
+                Assert.Equal(i, bm.ReadInt());
+            }
+        }
+
+        [Theory]
+        [InlineData(new byte[] {11, 22})]
+        public void TestReadIntFromExhaustedByteManipulator(byte[] sample)
+        {
+            var bm = ByteManipulator.Create(sample);
+            Assert.Throws<InvalidOperationException>(() => bm.ReadInt());
+        }
+        
+        [Theory]
+        [InlineData(new float[]{0, 0.1f, 0.2f, 0.3f}, new float[]{0, 0.1f, 0.2f, 0.3f})]
+        [InlineData(new float[]{4000.0f, 3600.1f, 3200.2f}, new float[]{4000.0f, 3600.1f, 3200.2f})]
+        public void TestReadFloat(float[] samples, float[] expected)
+        {
+            List<byte> sampleBytes = new List<byte>();
+            foreach (var t in samples)
+            {
+                var bytes = BitConverter.GetBytes(t);
+                for (var j = 0; j < 4; j++)
+                {
+                    sampleBytes.Add(bytes[j]);
+                }
+            }
+            Assert.Equal(samples.Length * 4, sampleBytes.Count);
+            
+            ByteManipulator bm = ByteManipulator.Create(ByteManipulator.Encode(sampleBytes.ToArray()));
+
+            foreach (var i in expected) 
+            { 
+                Assert.Equal(i, bm.ReadFloat());
+            }
+            
+        }
+        
+        [Theory]
+        [InlineData("ZmZmPw==", 0.9f)]
+        public void TestReadFloatFromRaw(string? sample, float? expected)
+        {
+            var sampleBytes =  Convert.FromBase64String(sample);
+            var bm = ByteManipulator.Create(ByteManipulator.Encode(sampleBytes));
+        
+            Assert.Equal(expected, bm.ReadFloat());
+        }
+        
+        [Theory]
+        [InlineData(new byte[] {11, 22})]
+        public void TestReadFloatFromExhaustedByteManipulator(byte[] sample)
+        {
+            var bm = ByteManipulator.Create(sample);
+            Assert.Throws<InvalidOperationException>(() => bm.ReadFloat());
         }
     }
 }
