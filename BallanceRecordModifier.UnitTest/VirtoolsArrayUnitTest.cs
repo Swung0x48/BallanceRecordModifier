ff6508a3f85a99661f97b3e18348475db0124e72
Swung0x48 <swung0x48@outlook.com>
2020-08-31 23:53:36 +0800

    99% unit test coverage on VirtoolsArray!

--- BallanceRecordModifier.UnitTest/VirtoolsArrayUnitTest.cs
+++ BallanceRecordModifier.UnitTest/VirtoolsArrayUnitTest.cs
@@ -17,8 +17,9 @@ namespace BallanceRecordModifier.UnitTest
             var va = VirtoolsArray.Create(bm);
             va.SetHeader(bm);
             va.PopulateCells(bm);
+            Assert.Equal("DB_Highscore_Lv01", va.SheetName);
         }
-        
+
         [Theory]
         [InlineData(
             "YiLB4gfG5kRGxySGwWOk7wz1svX19Sr19fUr9fX11dXV1eNnBgWGJKcGh4b1SvX19ePHB6dkRPUK9fX1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1gySv6WKGpgaEZ2T1+cv19eur9fXla/X19yv19eEL9fXzyvX17ar19f9q9fXpSvX1+wr19Q=="
@@ -31,8 +32,50 @@ namespace BallanceRecordModifier.UnitTest
             var va = VirtoolsArray.Create(bm);
             va.SetHeader(bm);
             va.PopulateCells(bm);
-            
+
             Assert.Throws<InvalidOperationException>(() => VirtoolsArray.Create(bm));
+        }
+
+        [Theory]
+        [InlineData(
+            "REJfTGV2ZWxmcmVpc2NoYWx0dW5nAFAAAAABAAAADAAAAP////9GcmVpZ2VzY2hhbHRldD8AAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==")]
+        public void TestCreateDB_Levelfreischaltung(string sample)
+        {
+            var sampleBytes = Convert.FromBase64String(sample);
+            var bm = ByteManipulator.Create(ByteManipulator.Encode(sampleBytes));
+            var va = VirtoolsArray.Create(bm);
+            va.SetHeader(bm);
+            va.PopulateCells(bm);
+            Assert.Equal("DB_Levelfreischaltung", va.SheetName);
+        }
+
+        [Theory]
+        [InlineData(
+            "YiLBw+dkB8enRPUU9fX1S/X19Qr19fXV1dXVoMdnhIeG9Sr19fVABadG5ulkx+lARiSGhqfN9Qr19fVDhgXposckxAYkZvUK9fX1Q4YF6SIGRkfEBiRm9Qr19fVDhgXpY4amZPUK9fX1Q4YF6SAHxuZk9Qr19fVDhgXpIMdkBmSG6UIGh/UK9fX1Q4YF6WMHpmTpQgaH9Qr19fUDp6SGJGTpQgaH6SDHZAZkB8enzfUK9fX1YwZEZONnBgWGJPVK9fX1QmfHhGZjBgWGJM31CvX19UxMTM319fX1YvX19YL19fWi9fX1wvX19c719fWM9fX19fX19acGh4b19fX19Q=="
+        )]
+        public void TestCreateDB_Options(string sample)
+        {
+            var sampleBytes = Convert.FromBase64String(sample);
+            var bm = ByteManipulator.Create(sampleBytes);
+            var va = VirtoolsArray.Create(bm);
+            va.SetHeader(bm);
+            va.PopulateCells(bm);
+            Assert.Equal("DB_Options", va.SheetName);
+            Assert.Equal(0.7f, va.Cells[0, 0]);
+        }
+
+        [Theory]
+        [InlineData(
+            "YiLBw+dkB8enRPUU9fX1S/X19Qr19fXV1dXVoMdnhIeG9Sr19fVABadG5ulkx+lARiSGhqfN9Qr19fVDhgXposckxAYkZvUK9fX1Q4YF6SIGRkfEBiRm9Qr19fVDhgXpY4amZPUK9fX1Q4YF6SAHxuZk9Qr19fVDhgXpIMdkBmSG6UIGh/UK9fX1Q4YF6WMHpmTpQgaH9Qr19fUDp6SGJGTpQgaH6SDHZAZkB8enzfUK9fX1YwZEZONnBgWGJPVK9fX1QmfHhGZjBgWGJM31CvX19UxMTM319fX1YvX19YL19fWi9fX1wvX19c719fWM9fX19fX19acGh4b19fX19Q=="
+        )]
+        public void TestToArray(string sample)
+        {
+            var sampleBytes = Convert.FromBase64String(sample);
+            var bm = ByteManipulator.Create(sampleBytes);
+            var va = VirtoolsArray.Create(bm);
+            va.SetHeader(bm);
+            va.PopulateCells(bm);
+            Assert.Equal(sampleBytes, va.ToByteArray());
         }
     }
 }
