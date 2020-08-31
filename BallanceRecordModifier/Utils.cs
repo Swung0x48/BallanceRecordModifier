1e6f7258efcab5c6c6b03e05a6f5c77f1135dcae
Swung0x48 <swung0x48@outlook.com>
2020-08-31 22:56:08 +0800

    Open browser to navigate to configuration page. (WIP)

---
+++ BallanceRecordModifier/Utils.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BallanceRecordModifier
+{
+    public class Utils
+    {
+        public static void GoToUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
