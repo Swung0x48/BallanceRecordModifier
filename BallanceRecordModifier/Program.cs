f092ea84964cafa46a084fdc8a758750b642bd76
Swung0x48 <swung0x48@outlook.com>
2020-09-02 00:24:50 +0800

    A test client for verify and performance benchmark.

--- BallanceRecordModifier/Program.cs
+++ BallanceRecordModifier/Program.cs
@@ -13,23 +13,24 @@ namespace BallanceRecordModifier
         {
             var sw = new Stopwatch();
             sw.Start();
-            var arr = await File.ReadAllBytesAsync("Database.9.tdb");
-            var bm = ByteManipulator.Create(arr);
+            var initMem = GC.GetTotalMemory(false);
+            var arr = await File.ReadAllBytesAsync("Database.tdb");
+            var bm = await ByteManipulator.Create(arr);
             
             var vaList = new List<VirtoolsArray>();
             try
             {
                 for (var i = 0; ; i++)
                 {
-                    var va = VirtoolsArray.Create(bm);
-                    va.SetHeader(bm);
-                    va.PopulateCells(bm);
+                    var va = await VirtoolsArray.Create(bm);
+                    await va.SetHeader(bm);
+                    await va.PopulateCells(bm);
             
                     vaList.Add(va);
             
                     string json = JsonConvert.SerializeObject(va, Formatting.Indented);
                     await File.WriteAllTextAsync($"{va.SheetName}.json", json);
-                    Console.Write($"\r{i + 1}/? Completed.");
+                    Console.Write($"\r{i + 1} arrays read.");
                 }
             }
             catch (InvalidOperationException e)
@@ -37,24 +38,26 @@ namespace BallanceRecordModifier
                 Console.WriteLine();
                 Console.WriteLine(e.Message);
                 Console.WriteLine($"Parse Completed. Elapsed {sw.ElapsedMilliseconds}ms.");
-                Console.WriteLine($"Memory usage {GC.GetTotalMemory(false) / 1024}KB.");
+                Console.WriteLine($"Memory usage {(GC.GetTotalMemory(false) - initMem) / 1024}KB.");
             }
+            
             sw.Reset();
             sw.Start();
             // Utils.GoToUrl("https://www.baidu.com");
-            
+
             for (var i = 0; i < vaList.Count; i++)
             {
                 using (var stream = new FileStream("Database.frankenstein.tdb", i == 0 ? FileMode.Truncate : FileMode.Append))
                 {
-                    stream.Write(ByteManipulator.Encode(vaList[i].ToByteArray()), 0, vaList[i].ToByteArray().Length);
+                    var byteArray = await vaList[i].ToByteArray();
+                    stream.Write(ByteManipulator.Encode(byteArray), 0, byteArray.Length);
                 }
-                Console.Write($"\r{i + 1}/{vaList.Count} Completed.");
+                Console.Write($"\r{i + 1}/{vaList.Count} arrays written.");
             }
             Console.WriteLine();
             Console.WriteLine($"Write Completed. Elapsed {sw.ElapsedMilliseconds}ms.");
-            Console.WriteLine($"Memory usage {GC.GetTotalMemory(false) / 1024}KB.");
-            await File.WriteAllBytesAsync("Database.new.tdb", bm.Array);
+            Console.WriteLine($"Memory usage {(GC.GetTotalMemory(false) - initMem) / 1024}KB.");
+            // await File.WriteAllBytesAsync("Database.new.tdb", bm.Array);
         }
     }
 }
