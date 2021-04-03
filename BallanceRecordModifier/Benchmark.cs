5602faa64dd158e3419da85c32223bfbe5640975
Swung0x48 <swung0x48@outlook.com>
2021-04-03 11:20:15 +0800

    Fully parallelized serialize and writing.

--- BallanceRecordModifier/Benchmark.cs
+++ BallanceRecordModifier/Benchmark.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -10,10 +11,12 @@ namespace BallanceRecordModifier
     [MemoryDiagnoser]
     public class Benchmark
     {
-        private string _pathToInputFile =
-            "path/to/Database.tdb";
-        private string _pathToOutputFile =
-            "/path/to/Database.out.tdb";
+        private static string _basePath =
+            "/Users/swung0x48/RiderProjects/BallanceRecordModifier/BallanceRecordModifier/bin/Debug/net5.0";
+        private static string _pathToInputFile =
+            _basePath + "/Database.10000xEnlarged.tdb";
+        private static string _pathToOutputFile =
+            _basePath + "/Database.out.tdb";
         
         [Benchmark]
         public async Task LegacyMethod()
@@ -32,8 +35,8 @@ namespace BallanceRecordModifier
             
                     vaList.Add(va);
             
-                    string json = JsonConvert.SerializeObject(va, Formatting.Indented);
-                    await File.WriteAllTextAsync($"{va.SheetName}.json", json);
+                    // string json = JsonConvert.SerializeObject(va, Formatting.Indented);
+                    // await File.WriteAllTextAsync($"{va.SheetName}.json", json);
 #if DEBUG 
                     Console.Write($"\r{i + 1} arrays read.");
 #endif
@@ -85,7 +88,7 @@ namespace BallanceRecordModifier
                         throw new EndOfStreamException();
                     var virtoolsArray = await VirtoolsArray.CreateAsync(tdbReader, false);
 #if DEBUG
-                    await Task.Run(() => Console.WriteLine($"Creating array #{i}: {virtoolsArray.SheetName}"));
+                    Console.Write($"\r[{i}/-] Reading array: {virtoolsArray.SheetName}");
 #endif
                     virtoolsArrays.Add(virtoolsArray);
                     var chunk = new byte[virtoolsArray.ChunkSize];
@@ -95,55 +98,92 @@ namespace BallanceRecordModifier
                     var memory = new ReadOnlyMemory<byte>(chunk);
                     populateTasks.Add(virtoolsArray.PopulateAsync(memory));
 #if DEBUG
-                    await Task.Run(() =>
-                        Console.WriteLine($"Starting populating array #{i} asynchronously: {virtoolsArrays[i - 1].SheetName}"));
+                    // await Task.Run(() =>
+                    //     Console.Write($"\rStarting populating array #{i} asynchronously: {virtoolsArrays[i - 1].SheetName}"));
 #endif
                 }
             }
             catch (EndOfStreamException)
             {
 #if DEBUG
-                Console.WriteLine("End of the stream reached.");
+                Console.WriteLine("\nEnd of the stream reached.");
 #endif
             }
 
 #if DEBUG
-            Console.WriteLine("Waiting all populating process to finish...");
+            Console.WriteLine("Waiting all parsing process to finish...");
 #endif
 
             Task.WaitAll(populateTasks.ToArray());
 #if DEBUG
-            Console.WriteLine("All populating process to finished!");
+            Console.WriteLine("Parse Completed.");
 #endif
 
-            virtoolsArrays.ForEach(e =>
-            {
-                string json = JsonConvert.SerializeObject(e, Formatting.Indented);
-                File.WriteAllText($"{e.SheetName}.json", json);
-            });
+            // virtoolsArrays.ForEach(e =>
+            // {
+            //     string json = JsonConvert.SerializeObject(e, Formatting.Indented);
+            //     File.WriteAllText($"{e.SheetName}.json", json);
+            // });
 
             var stream = new FileStream(_pathToOutputFile, FileMode.Truncate);
 
-            var chunkGeneratingTasks = new List<Task<byte[]>>();
-            virtoolsArrays.ForEach(async array =>
+            var serializingTasks = new BlockingCollection<Task<byte[]>>();
+            var chunksToWrite = new BlockingCollection<byte[]>();
+
+            var consumerTask = Task.Run(() =>
             {
+                var i = 0;
+                while (!chunksToWrite.IsCompleted)
+                {
 #if DEBUG
-                Console.WriteLine($"Preparing binary data to write for {array.SheetName}");
+                    Console.WriteLine($"[{++i}/{virtoolsArrays.Count}] Writing chunk to file...");
 #endif
-                chunkGeneratingTasks.Add(array.ToArrayAsync());
+                    stream.Write(chunksToWrite.Take());
+                }
+#if DEBUG
+                Console.WriteLine("All chunks written!");
+#endif
+            });
+            
+            var waitingProducerFinishTask = Task.Run(() =>
+            {
+                var i = 0;
+                while (!serializingTasks.IsCompleted)
+                {
+                    var task = serializingTasks.Take();
+#if DEBUG
+                    Console.WriteLine($"[{++i}/{virtoolsArrays.Count}] Waiting task to finish...");
+#endif
+                    task.Wait();
+#if DEBUG
+                    Console.WriteLine($"[{i}/{virtoolsArrays.Count}] Serializing task finished...");
+#endif
+                    chunksToWrite.Add(task.Result);
+                }
+                chunksToWrite.CompleteAdding();
+#if DEBUG
+                Console.WriteLine("All serializing tasks finished!");
+#endif
             });
 
-            foreach (var task in chunkGeneratingTasks)
+            var producerTask = Task.Run(() =>
             {
+                for (var i = 0; i < virtoolsArrays.Count; i++)
+                {
 #if DEBUG
-                Console.WriteLine("Waiting preparing process to finish...");
+                    Console.WriteLine($"Serializing array: {virtoolsArrays[i].SheetName}");
 #endif
-                task.Wait();
+                    serializingTasks.Add(virtoolsArrays[i].ToArrayAsync());
+                }
+
+                serializingTasks.CompleteAdding();
 #if DEBUG
-                Console.Write("Writing current fragment to file...");
+                Console.WriteLine("All serializing tasks fired!");
 #endif
-                stream.Write(task.Result);
-            }
+            });
+
+            consumerTask.Wait();
+
             stream.Close();
         }
     }
--- BallanceRecordModifier/Program.cs
+++ BallanceRecordModifier/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BenchmarkDotNet.Running;
-using Newtonsoft.Json;
 
 namespace BallanceRecordModifier
 {
     class Program
     {
+#if RELEASE
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+#endif
+        
+#if DEBUG
         static async Task Main(string[] args)
         {
-#if RELEASE
-            BenchmarkRunner.Run<Benchmark>();
-#endif
-#if DEBUG
              var benchmark = new Benchmark();
-             await benchmark.LegacyMethod();
+             // await benchmark.LegacyMethod();
              await benchmark.NewMethod();
-#endif
         }
+#endif
+
     }
 }
